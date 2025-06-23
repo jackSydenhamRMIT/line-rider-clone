3e5e6ed99b90ecb27367fc3d6a087cea1e9046c5
JSydAstral <jack.sydenham@astral.com.au>
2025-06-23 20:02:03 +1000

    2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Editor from './components/Editor'
+import Editor from './Components/Editor'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
