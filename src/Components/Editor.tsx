c62cc89b94cbca5e2408cde41f2d99337be39dae
JSydAstral <jack.sydenham@astral.com.au>
2025-06-23 20:01:25 +1000

    imp change

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -1,7 +1,7 @@
 import DrawOverlay from "./DrawOverlay";
-import "./Editor.css"
 import PhysicsCanvas from "./PhysicsCanvas";
 import Toolbar from "./Toolbar";
+
 
 export default function Editor() {
   return (
